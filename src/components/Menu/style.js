1ff42ce4b58d43a601ddf1d953ceda4d5acfcd77
Familia <Familia@DBORA>
2020-08-01 23:55:50 -0300

    customização de botão

--- src/components/Menu/style.js
+++ src/components/Menu/style.js
@@ -36,35 +36,33 @@ export const MenuWrapper = styled.nav`
 `;
 
 export const ButtonLink = styled(Button)`
-border: 1px solid var(--white);
-border-radius: 4px;
-color: var(--white);
-font-size: 16px;
-font-weight: bold;
-padding: 16px 24px;
-text-decoration: none;
-transition: opacity .3s;
-&:hover,
-&:focus
-{
-    opacity: .5;
-}
 
- @media(max-width: 800px){
-
-    position: fixed;
-    left: 0;
-    right: 0;
-    border: 0;
-    bottom: 0;
-    background-color: var(--primary);
+    border: 1px solid var(--white);
+    border-radius: 4px;
     color: var(--white);
-    font-size: 15px;
-    /* padding: 10px 18px; */
+    font-size: 20px;
+    font-weight: bold;
+    padding: 16px 24px;
+    text-decoration: none;
+    transition: opacity .3s;
+    &:hover,
+    &:focus
+    {
+        opacity: .5;
+    }
 
-} 
-/*@media(max-width: 380px){
-    font-size: 12px;
-    padding: 8px 14px;
-    }  */
+    @media(max-width: 800px){
+
+        position: fixed;
+        left: 0;
+        right: 0;
+        border: 0;
+        bottom: 0;
+        background-color: var(--primary);
+        color: var(--white);
+        font-size: 25px;
+        text-align: center;
+        border-radius: 0;
+
+    } 
 `;

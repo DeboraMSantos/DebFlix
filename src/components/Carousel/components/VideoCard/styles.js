ee47d5347114332ce524bdebaa6915873c0e54db
Familia <Familia@DBORA>
2020-08-02 12:42:07 -0300

    mudança de cor

--- src/components/Carousel/components/VideoCard/styles.js
+++ src/components/Carousel/components/VideoCard/styles.js
@@ -18,13 +18,12 @@ export const VideoCardContainer = styled.a`
   display: flex;
   align-items: flex-end;
   padding: 16px;
-
   transition: opacity .3s;
   &:hover,
   &:focus {
     opacity: .5;
   }
-  
+ 
   &:not(:first-child) {
     margin-left: 20px;
   }

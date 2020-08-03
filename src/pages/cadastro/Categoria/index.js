2c36eaf884af6961298f59cbfa5fe88bac07210c
Familia <Familia@DBORA>
2020-08-03 13:01:48 -0300

    ajuste titulo

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -92,8 +92,8 @@ function CadastroCategoria() {
       )}
       <ul>
         {categorias.map((categoria) => (
-          <li key={`${categoria.nome}`}>
-            {categoria.nome}
+          <li key={`${categoria.titulo}`}>
+            {categoria.titulo}
           </li>
         ))}
       </ul>

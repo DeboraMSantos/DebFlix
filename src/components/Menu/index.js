4ede1d9810bc3e66f38d19ffc4e73b750836e5ef
Familia <Familia@DBORA>
2020-08-03 02:54:20 -0300

    a

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
 import Jogo from '../../assets/img/jogo.png';
 import Nerd from '../../assets/img/nerd.png';
-import Animais from '../../assets/img/nerd.png';
+import Animais from '../../assets/img/animal.png';
 import Filme from '../../assets/img/filme.png';
 import Husky from '../../assets/img/husky.gif';
 import { LogoImage, MenuWrapper, ButtonLink } from './style';
